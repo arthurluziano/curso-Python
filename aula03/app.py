4cc23d0cdb19614063e33ba963bb23fdefe148a6
Arthur Luziano <luzianoarthur@gmail.com>
2024-02-21 16:55:51 -0300

    add aula03

---
+++ aula03/app.py
@@ -0,0 +1,33 @@
+num1 = 5
+num2 = 3.5
+
+a = float(num1) # Transforma o valor para o tipo FLOAT.
+b = int(num2) # Transforma o valor para o tipo INT.
+
+a = "5"
+b = "3.5"
+
+# Operações Matemáticas:
+# print(num1 + num2)
+# print(num1 - num2)
+# print(num1 * num2)
+# print(num1 / num2)
+# print(num1 // num2)
+# print(num1 ** num2)
+# print(num1 % num2)
+
+numeroArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# Funções Matemáticas:
+# print(abs(-15)) # Pega o valor absoluto.
+# print(pow(3, 3)) # Realiza a potenciação entre os valores.
+# print(max(numeroArray)) # Pega o valor máximo.
+# print(min(numeroArray)) # Pega o valor mínimo.
+# print(round(8.5)) # Arredonda o valor para cima (> 5) ou para baixo (<= 5).
+
+# Funções da biblioteca MATH:
+import math
+
+print(math.floor(8.8)) # Arredonda o valor SEMPRE para baixo.
+print(math.ceil(8.1)) # Arredonda o valor SEMPRE para cima.
+print(math.sqrt(9)) # Retorna a raiz quadrada do valor inserido.
