f65f349e27fe913f259ed1be35a2611cf292a7d2
Arthur Luziano <luzianoarthur@gmail.com>
2024-02-21 16:34:49 -0300

    add aula02

---
+++ aula02/app.py
@@ -0,0 +1,12 @@
+nome = "Arthur"
+idade = 19
+altura = 1.78
+hobbie = "programação"
+
+print(f"Meu nome é {nome}!")
+print(f"Eu gosto de {hobbie}.")
+print(f"Eu tenho {idade} anos.")
+
+idade = 21
+
+print(f"{nome} gosta de {hobbie}, tem {idade} anos e {altura}m de altura.")
