1f77e0cca042a0193e5612e1b738372aa0192a6e
Arthur Luziano <luzianoarthur@gmail.com>
2024-02-22 11:49:59 -0300

    add aula04

---
+++ aula04/app.py
@@ -0,0 +1,24 @@
+minha_string = "qualquer texto"
+x = "texto" in minha_string
+
+# print(minha_string.upper()) # Transforma todos os caracteres para maiúsculo.
+# print(minha_string.lower()) # Transforma todos os caracteres para minúsculo.
+# print(minha_string.isupper()) # Checa se todos os caracteres estão em maiúsculo.
+# print(minha_string.islower()) # Checa se todos os caracteres estão em minúsculo.
+# print(minha_string.strip()) # Remove todos os espaços anteriores e posteriores de uma String.
+# print(minha_string.replace("u", "U", 1)) # Altera determinada letra/palavra de uma String.
+# print(len(minha_string)) # Retorna o número de caracteres existentes na String.
+# print(minha_string[2:5]) # Retorna um ou mais caracteres específicos da String.
+# print(minha_string.index("u")) # Retorna o índice de determinado elemento da String.
+# print(x) # Verifica se um elemento existe dentro da String.
+
+y = """
+linha 1,
+linha 2,
+linha 3.
+"""
+
+z = "linha 1, \nlinha 2, \nlinha 3."
+
+print(y)
+print(z)
