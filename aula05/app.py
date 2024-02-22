15eb6e9b6ef95a570c23eab708ff90544f8217ef
Arthur Luziano <luzianoarthur@gmail.com>
2024-02-22 15:49:30 -0300

    add aula05

---
+++ aula05/app.py
@@ -0,0 +1,16 @@
+# nome = input("Digite seu nome: ")
+# idade = int(input(f"Qual a sua idade {nome}?\n"))
+
+# print(f"Bem vindo, {nome}! Sua idade é {idade} anos.")
+
+"""
+Exemplo de comentário em linha.
+"""
+
+print("----- Calculadora de Soma ------")
+num1 = float(input("Digite um número: "))
+num2 = float(input("Digite outro número: "))
+res = num1 + num2
+
+print("----- Resultado -----")
+print(f"A soma entre {num1} e {num2} é igual a: {res} ")
