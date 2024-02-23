07a83ed68a4357fbf5051c9469c635e155a96576
Arthur Luziano <luzianoarthur@gmail.com>
2024-02-23 06:47:02 -0300

    update aula06

--- aula06/app.py
+++ aula06/app.py
@@ -2,13 +2,39 @@ cornos = ["Tur", "Fab", "Dan", "Dragão", "Huguinho", "Gabs"]
 #           0      1      2         3         4         5
 #          -6     -5     -4        -3        -2        -1
 
-# print(cornos[-1]) # Retorna o último valor (Ordem de trás pra frente).
-# print(cornos[2]) # Retorna o valor do índice 2.
-# print(cornos[1:]) # Retorna os valores do índice 1 até o fim da coleção.
+# print(cornos[-1]) # Retorna o último valor da lista.
+# print(cornos[2]) # Retorna o valor do índice 2 da lista.
+# print(cornos[1:]) # Retorna os valores do índice 1 até o fim da lista.
 # print(cornos[1:4]) # Retorna os valores do índice 1 até o índice 4 (excluindo o valor do 4° índice).
 
-cornos[3] = "Levi"
+# cornos[3] = "Levi" # Altera o valor do índice indicado.
 
-cornos.extend(["João", "Pedro"]) # Concatena uma nova lista na lista especificada.
-cornos.append("Bruno") # Adiciona um valor à lista.
-print(cornos)
+# cornos.extend(["João", "Pedro"]) # Concatena uma nova lista à lista especificada.
+# cornos.append("Bruno") # Adiciona um valor ao final da lista.
+# cornos.insert(1, "Bruno") # Adiciona um valor ao índice indicado da lista.
+# cornos.pop() # Remove o último valor da lista.
+# cornos.remove("Bruno") # Remove um valor específico da lista.
+# cornos.clear() # Remove todos os valores da lista.
+
+# print(cornos.index("Levi")) # Retorna o índice do valor especificado.
+# print(cornos.count("Levi")) # Retorna o número de vezes que o valor se repete na lista.
+
+# print(cornos)
+
+# idade_cornos = [20, 22, 19, 24, 21, 23]
+# idade_cornos.sort() # Ordena a lista (Ascendente).
+# idade_cornos.reverse() # Reverte a ordem dos valores da lista.
+
+# print(idade_cornos)
+
+# cornos2 = cornos # Utiliza a lista "cornos" como referência.
+# print(cornos2)
+
+# Portanto, tudo que acontece na lista "cornos" afeta a nova lista "cornos2".
+# cornos.remove("Levi")
+# print(cornos2)
+
+# cornos3 = cornos.copy() # Cria uma nova lista copiando os itens da lista "cornos".
+
+# coordenadas = (-49, -36) # Cria uma Tupla (seus valores são IMUTÁVEIS).
+# print(coordenadas)
