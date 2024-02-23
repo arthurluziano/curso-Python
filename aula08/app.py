43317da634a082d94c7af702193c0f42d5c8fd84
Arthur Luziano <luzianoarthur@gmail.com>
2024-02-23 07:19:12 -0300

    add aula08

---
+++ aula08/app.py
@@ -0,0 +1,45 @@
+# fazFrio = True
+
+# if fazFrio:
+#   print("Vestir um casaco")
+# else:
+#   print("Vestir camiseta")
+
+# tenhoSede = True
+# tenhoFome = False
+
+# or -> Ou
+# and -> E
+# not() -> Negação
+
+# if tenhoFome or tenhoSede: # Exemplo de ||
+#   print("Ir à cozinha")
+# else:
+#   print("Ficar deitado")
+
+# if tenhoFome and tenhoSede:
+#   print("Ir à cozinha")
+# else:
+#   print("Ficar deitado")
+
+# if tenhoSede and tenhoFome: # Exemplo de &&
+#   print("Pegar comida e bebida")
+# elif tenhoSede: # Abreviação de "else if"
+#   print("Pegar apenas bebida")
+# else:
+#   print("Ficar deitado")
+
+# == -> Igual
+# != -> Diferente
+# > -> Maior
+# >= -> Maior ou Igual
+# < -> Menor
+# <= -> Menor ou Igual
+
+# num1 = 5
+# num2 = 5
+
+# if num1 == num2:
+#   print(f"Os números {num1} e {num2} são iguais!")
+# else:
+#   print(f"Os números {num1} e {num2} não são iguais!")
